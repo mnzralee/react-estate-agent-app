df51f996af36dce731e2faeb862fc5b9bd23d948
mnzralee <133229508+mnzralee@users.noreply.github.com>
2024-12-25 17:35:40 +0530

    app setup

---
+++ react-estate-agent/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import "react-widgets/styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './components/Header/Header'
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ react-estate-agent/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+import "./Header.css";
+import SearchBar from "../SearchBar/SearchBar";
+
+function Header() {
+    return (
+        <header>
+            <div className="d-flex justify-content-center align-items-center py-4">
+                <h1 className="header-1">Your Next HOME</h1>
+            </div>
+            <SearchBar />
+        </header>
+    );
+}
+
+export default Header;
